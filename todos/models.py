78b43e06580a1a1aa1e5f545bc890d1abcf98ccf
Chiran Rimal <chiranrimal803@gmail.com>
2025-03-03 17:02:51 +0545

    models.py updated with task choices and priority with duedate

--- todos/models.py
+++ todos/models.py
@@ -6,5 +6,20 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    STATUS_CHOICE = {
+        ('pending', 'pending'),
+        ('in_progress', 'In progress'),
+        ('completed', 'completed'),
+    }
+    PRIORITY_CHOICE = {
+        ('low', 'low'),
+        ('medium', 'medium'),
+        ('high', 'high'),
+    }
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='pending')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICE, default='low')
+    due_date = models.DateTimeField(null=True, blank=True)
+
     def __str__(self):
         return self.title
